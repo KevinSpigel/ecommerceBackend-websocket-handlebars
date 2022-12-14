b3554c81240d8821f357004f189345a038cb12ca
KevinSpigel <kevinspigel@gmail.com>
2022-12-14 20:55:00 +0100

    code format with Prettier extension

--- ProductManager.js
+++ ProductManager.js
@@ -1,106 +1,106 @@
-const fs = require('fs/promises');
-const { existsSync } = require('fs');
-
-
+const fs = require("fs/promises");
+const { existsSync } = require("fs");
 
 class ProductManager {
+  static lastProductId = 0;
 
-    static lastProductId = 0;
+  constructor(path) {
+    this.path = path;
+  }
 
-    constructor(path) {
-        this.path = path;
+  async loadProducts() {
+    if (existsSync(this.path)) {
+      const dataProducts = await fs.readFile(this.path, "utf-8");
+      const allProducts = JSON.parse(dataProducts);
+      allProducts.forEach((product) => {
+        product.id = Number(product.id);
+      });
+      return allProducts;
+    } else {
+      return [];
     }
+  }
 
-    async loadProducts() {
-        if (existsSync(this.path)) {
-            const dataProducts = await fs.readFile(this.path, 'utf-8');
-            const allProducts = JSON.parse(dataProducts);
-            allProducts.forEach(product => {product.id=Number(product.id)
-            });
-            return allProducts;
-        }
-        else {
-            return [];
-        }
+  async saveProducts(allProducts) {
+    await fs.writeFile(this.path, JSON.stringify(allProducts, null, "\t"));
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    const data = await this.getProducts();
+
+    if (
+      !this.data.find((product) => product.code === code) &&
+      title &&
+      description &&
+      code &&
+      price &&
+      thumbnail &&
+      stock
+    ) {
+      ProductManager.lastProductId++;
+
+      const newProduct = {
+        id: ProductManager.lastProductId,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+
+      data.push(newProduct);
+
+      await this.saveProducts(data);
     }
+  }
 
-    async saveProducts(allProducts) {
-        await fs.writeFile(this.path, JSON.stringify(allProducts, null, '\t'));
+  async getProducts() {
+    if (existsSync(this.path)) {
+      return await this.loadProducts();
+    } else {
+      console.error("Not found");
     }
+  }
 
-
-    async addProduct(title, description, price, thumbnail, code, stock) {
-
-        const data = await this.getProducts();
-
-        if (!this.data.find(product => product.code === code) && (title && description && code && price && thumbnail && stock)) {
-            ProductManager.lastProductId++;
-
-            const newProduct = {
-                id: ProductManager.lastProductId,
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock,
-            };
-
-            data.push(newProduct);
-
-            await this.saveProducts(data);
-        }
+  async getProductById(id) {
+    const allProducts = await this.getProducts();
+    const productById = allProducts.find((product) => product.id === id);
+    if (productById) {
+      return productById;
+    } else {
+      console.error("Not found");
     }
+  }
 
+  async updateProduct(id, newProductProperties) {
+    const products = await this.getProducts();
+    const foundProduct = await this.getProductById(id);
 
-    async getProducts() {
-        if (existsSync(this.path)) {
-            return await this.loadProducts();
-        }
-        else {
-            console.error("Not found");
-        }
-    };
+    const productUpdated = { ...foundProduct, ...newProductProperties };
 
-    async getProductById(id) {
-        const allProducts = await this.getProducts();
-        const productById = allProducts.find((product) => product.id === id);
-        if (productById) {
-            return productById
-        } else {
-            console.error("Not found")
-        }
-    }
+    const updatedList = products.map((elem) => {
+      if (elem.id === productUpdated.id) {
+        return productUpdated;
+      } else {
+        return elem;
+      }
+    });
 
+    const stringList = await fs.writeFile(
+      this.path,
+      JSON.stringify(updatedList, null, "\t")
+    );
 
-    async updateProduct(id, newProductProperties) {
-        const products = await this.getProducts()
-        const foundProduct = await this.getProductById(id)
+    return stringList;
+  }
 
-        const productUpdated = { ...foundProduct, ...newProductProperties }
-
-        const updatedList = products.map(elem => {
-            if (elem.id === productUpdated.id) {
-                return productUpdated
-            } else {
-                return elem
-            }
-        })
-
-        const stringList = await fs.writeFile(this.path, JSON.stringify(updatedList, null, '\t')) 
-
-        return stringList
-    }
-
-
-    async deleteProduct(id) {
-        const products = await this.getProducts();
-        const filteredById = products.filter((product) => product.id !== id);
-        this.saveProducts(filteredById);
-        return filteredById
-    }
-
-
-};
+  async deleteProduct(id) {
+    const products = await this.getProducts();
+    const filteredById = products.filter((product) => product.id !== id);
+    this.saveProducts(filteredById);
+    return filteredById;
+  }
+}
 
 module.exports = ProductManager;
--- index.js
+++ index.js
@@ -1,20 +1,14 @@
+const ProductManager = require("./ProductManager");
 
-const ProductManager = require('./ProductManager');
+const ecommerce = new ProductManager("./database/productsDataBase.json");
 
-const ecommerce = new ProductManager ('./database/productsDataBase.json');
-
-const fileProcess = async () =>{
-    try{
-
-
-        const testUpdate = await ecommerce.updateProduct(1,{code:"24"})
-        console.log(testUpdate)
-
-    }
-
-    catch (error){
-        throw new Error (error) 
-    }
+const fileProcess = async () => {
+  try {
+    const testUpdate = await ecommerce.updateProduct(1, { code: "24" });
+    console.log(testUpdate);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 fileProcess();
